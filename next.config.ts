b6ffebd0bad014e94a03a0b2e325f6651be38049
Stark Coder <87575519+esurajpandey@users.noreply.github.com>
2026-04-18 21:23:20 +0530

    Prod releasing (#27)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        // This makes your Render backend look like it's part of your Vercel frontend
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_BASE_URL as string,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
